eb01451d62b520b19d79af4b54909369174a24ee
Nilesh Gawli <nbgawli@gmail.com>
2024-02-27 20:53:39 +0530

    style: Add style to home screen

--- src/Components/Screens/Home.jsx
+++ src/Components/Screens/Home.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 
 export default function Home() {
+
   return (
     <>
 <div className="h-screen w-full relative">
-  <div className="absolute inset-0 bg-black opacity-45"></div>
+  <div className="absolute inset-0 bg-black opacity-45">
+
+      <Navbar/>
+
+  </div>
   <div className="h-full w-full bg-home bg-no-repeat bg-cover bg-end shadow-md m:bg-center bg-fixed">
   </div>
 </div>
     </>
   )
 }
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-between items-center font-Onest p-6">
+        <h1 className="text-5xl font-bold text-white border-b-2 border-white">Fraud <br /> Detection</h1>
+
+        <div className="flex justify-between items-center space-x-4 text-lg text-white self-start">
+          <a className="text-white " href="/login">
+            Login
+          </a>
+          <a className="text-white " href="/signup">
+            Signup
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
